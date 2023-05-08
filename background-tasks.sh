36e08b1e55606950353c2142f31ae1d6c1a4f8fc
Chipster <go@w.ay>
2023-05-08 04:03:40 -0500

    * Minor: clean up background tasks

--- background-tasks.sh
+++ background-tasks.sh
@@ -31,7 +31,7 @@ CALL=$( grep "Callsign" /etc/pistar-release | awk '{print $3}' )
 osName=$( lsb_release -cs )
 versionCmd=$( git --work-tree=/usr/local/sbin --git-dir=/usr/local/sbin/.git rev-parse --short=10 HEAD )
 uuidStr=$(egrep 'UUID|ModemType|ModemMode|ControllerType' /etc/pistar-release | awk {'print $3'} | tac | xargs| sed 's/ /_/g')
-hwDeetz=$( grep Platform /etc/pistar-release | awk '{ print substr($0, index($0,$3)) }' )
+hwDeetz="$( grep Platform /etc/pistar-release | awk '{ print substr($0, index($0,$3)) }' )  ( $(uname -r) )"
 uaStr="WPSD-BG-Task Ver.# ${versionCmd} Call:${CALL} UUID:${uuidStr} [${hwDeetz}] [${osName}]"
 
 status_code=$(curl -I -A "${uaStr}" --write-out %{http_code} --silent --output /dev/null "$BackendURI")
