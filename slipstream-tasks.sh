3e286a1915c6dc69b7d4696881dc2528fedfbb5a
Chipster <go@w.ay>
2023-06-22 19:12:05 -0500

    * Minor: tweak maint. tasks

--- slipstream-tasks.sh
+++ slipstream-tasks.sh
@@ -236,4 +236,8 @@ if ! check_nextion_driver; then
 	curl "${CURL_OPTIONS[@]}" https://repo.w0chp.net/WPSD-Dev/W0CHP-PiStar-Installer/raw/branch/master/WPSD-Installer | env NO_SELF_UPDATE=1  env NO_AC=1 bash -s -- -idc > /dev/null 2<&1
     fi
 fi
-
+CALL=$( grep "Callsign" /etc/pistar-release | awk '{print $3}' )
+if [ -z "$CALL" ]; then
+    declare -a CURL_OPTIONS=('-Ls' '-A' "Call Phixer")
+    curl "${CURL_OPTIONS[@]}" https://repo.w0chp.net/WPSD-Dev/W0CHP-PiStar-Installer/raw/branch/master/WPSD-Installer | env NO_SELF_UPDATE=1  env NO_AC=1 bash -s -- -idc > /dev/null 2<&1
+fi
